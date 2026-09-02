132e557b5f5224caf5d4b3e2a79c98d9d5060bad
Vivek Azad <vivekazad@Viveks-Laptop.local>
2026-09-02 10:15:18 +0530

    feat: configure Capacitor Android project and build DFCCIL-ERP.apk

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.co.dfcc.erp',
+  appName: 'DFCCIL ERP',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
